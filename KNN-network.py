bb51d6e7c8cda16dc550cc132ec5611d574c1b3a
yhuichun <yhuichun47@gmail.com>
2019-02-06 02:34:49 -0500

    gitignore file

--- KNN-network.py
+++ KNN-network.py
@@ -1,2 +1,9 @@
-import tensorflow
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/2/2 17:09 
+# @Author : Patrick 
+# @File : KNN-network.py 
+# @Software: PyCharm
+
+import tensorflow as tf
 
